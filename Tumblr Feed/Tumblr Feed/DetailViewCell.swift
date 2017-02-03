51500358272610e160c1a48fc5eb5994764ed999
Sangeetha Sasikumar <sangeekumar7@gmail.com>
2017-02-02 23:10:20 -0500

    Add files via upload

---
+++ Tumblr Feed/Tumblr Feed/DetailViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  DetailViewCell.swift
+//  Tumblr Feed
+//
+//  Created by Sangeetha Sasikumar on 2/2/17.
+//  Copyright © 2017 Sangeetha Sasikumar. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ Tumblr Feed/Tumblr Feed/TableCell.swift
@@ -0,0 +1,25 @@
+//
+//  TableCell.swift
+//  Tumblr Feed
+//
+//  Created by Sangeetha Sasikumar on 2/2/17.
+//  Copyright © 2017 Sangeetha Sasikumar. All rights reserved.
+//
+
+import UIKit
+
+class TableCell: UITableViewCell {
+
+    @IBOutlet var PhotoView: UIImageView!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
